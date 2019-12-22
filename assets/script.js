bce6426d013d9e3026f2ceb839da7af53e592704
nathan olenik <nolenik23@outlook.com>
2019-12-22 16:10:51 -0500

    working ajax

--- assets/script.js
+++ assets/script.js
@@ -8,29 +8,40 @@ $("#searchBtn").on("click", function (e) {
   // grabds the city the entered, after a submit button is made i can turn it into a onlick function
 
   var APIKey = "298a4f435bb40084f3affdac067f0650";
-  
-   var queryURL = `http://api.openweathermap.org/data/2.5/forecast?units=imperial&appid=298a4f435bb40084f3affdac067f0650&q=${location}`
+
+  var queryURL = `http://api.openweathermap.org/data/2.5/forecast?units=imperial&appid=298a4f435bb40084f3affdac067f0650&q=${location}`
 
   $.ajax({
     url: queryURL,
     method: "GET"
   })
+
+
     .then(function (response) {
-      console.log(response);
-      
+      // console.log(response);
+
+      var fiveDayForecast = []
+
+      response.list.forEach(function (obj, i) {
+        if (i % 8 === 0) {
+          fiveDayForecast.push(response.list[i + 7])
+        }
+      })
+
+      fiveDayForecast.forEach(function (obj, i) {
+        var row = $(".weatherCard")[i]
+        // update date to corresponding weatherCard
+        var weatherDate = $(row).find(".date")
+        weatherDate.text(obj.dt_txt.split(" ")[0])
+          console.log(obj)
+        // update icon, temp, humidity, and windspeed to corresponding weatherCard
+        var row2 = $(".weatherCard")[i]
+        var weatherIcon = $(row2).find(".weather-icon")
+        weatherIcon.attr("src", `http://openweathermap.org/img/wn/${obj.weather[0].icon}.png`)
 
 
 
-      var days = [0, 7, 15, 23, 31]
-      var titles = ["Tonight", "Tomorrow", "Day Three", "Day Four", "Day Five"]
-      for (var i = 0; i < days.length; i++) {
-
-        var cityName = response.city.name;
-        var temphigh = response.list[i].main.temp_max;
-        var templow = response.list[i].main.temp_low;
-        var windSpeed = response.list[i].wind.speed; 
-        
-      
+      });
 
 
 
@@ -44,7 +55,13 @@ $("#searchBtn").on("click", function (e) {
 
 
 
-      }
-    })
 
+
+
+
+
+
+
+
+    });
 });
